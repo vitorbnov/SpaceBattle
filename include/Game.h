4e2758c96f39ebd9150a52a26e7cf0120ea5e525
vitorbnov <vitorbnov@gmail.com>
2015-09-22 17:37:34 -0300

    Game pause/continue function, setup/clear levels, game mode variables

--- include/Game.h
+++ include/Game.h
@@ -1,40 +1,68 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <random>
 #include "../LUtil.h"
 #include "Polygon.h"
 #include "Aircraft.h"
 #include "Asteroid.h"
+#include "Circle.h"
 
-#define SHOTS_MAX 64
+#define SHOTS_MAX 128
+
+enum gameType{
+    CAMPAIGN=0,
+    ARCADE,
+    CHALLENGE
+};
+
 
 class Game{
     public:
         Game();
 
         void setPlayer(Aircraft*);
+        void pause();
+        void continuee();
+        void pauseOrContinue();
+
         Aircraft *getPlayer();
+        bool isPaused();
+
+        void resetPlayer();
+
+        void setUpLevel(int, int);
+        void advanceLevel();
+        void clearLevel();
 
         void shoot(Aircraft*);
         void crash(Asteroid*);
         void removeShot(Shot*);
-        void destroy(Polygon*);
+        void moveToDestroyed(GameObj*);
         void assignItem(Aircraft*, Item*);
-        void iterate(bool, bool, bool, bool);
+
+        void iterate();
+
         void draw(void);
         void printWord(float, float, const char*, void*);
+
         virtual ~Game();
     protected:
     private:
         long iterations;
-        //Aircraft *aircrafts[3];
+
+        int game_mode;
+        int level;
+        bool paused;
         Aircraft *player;
+        Aircraft *player_dead;
         std::vector<Asteroid*> asteroids;
         //Shot *shot;
         std::vector<Item*> items;
         std::vector<Shot*> shots;
         std::vector<Shot*> to_shot;
-        std::vector<Polygon*> destroyed;
+        std::vector<GameObj*> destroyed;
+        //std::poisson_distribution<long> enemies_comming();
         int interceptions;
         //vector<Polygon> enemies;
 };
--- src/Game.cpp
+++ src/Game.cpp
@@ -1,31 +1,26 @@
 #include <iostream>
 #include <cstdlib>
 #include <cstring>
+#include <random>
+#include "../LUtil.h"
 #include "Game.h"
 #include "Polygon.h"
 #include "Aircraft.h"
 
 Game::Game(){
     iterations = 0;
-    interceptions = 0;
-    //aircrafts[0] = new Aircraft(0);
-    //aircrafts[1] = new Aircraft(1);
-    //aircrafts[2] = new Aircraft(2);
-    //player = aircrafts[0];
+    paused = false;
+
+    player_dead = NULL;
     player = new Aircraft(0);
     player->centralize();
-    player->setColor(1, 1, 0);
+    player->setColor(1, 0, 0);
 
-    //player->upgradeStat(BACK);
-    //player->upgradeStat(DUAL);
-    //player->upgradeStat(FAST);
-    //player->upgradeStat(FAST);
+    //player->setAcceleration(0.11);
+    //player->setDelayTimeShooting(10);
+    //player->upgradeStat(HANDLING);
 
-    //aircrafts[0]->setColor(1, 0.5, 0);
-    //aircrafts[1]->setColor(1, 0.5, 0);
-    //aircrafts[2]->setColor(1, 0.5, 0);
-
-    Asteroid *asteroid = new Asteroid(BIG);
+/*    Asteroid *asteroid = new Asteroid(BIG);
     //asteroid->setColor(0, 1, 0);
     asteroid->setSpeed(Dot(0.5, 1));
     asteroid->setHandling(1);
@@ -33,60 +28,175 @@ Game::Game(){
     asteroid->centralize();
     asteroids.push_back(asteroid);
 
+    asteroid = new Asteroid(BIG);
+    //asteroid->setColor(0, 1, 0);
+    asteroid->setSpeed(Dot(-0.5, 1));
+    asteroid->setHandling(1);
+    asteroid->setPosition(Dot(0, -100));
+    asteroid->centralize();
+    asteroids.push_back(asteroid);
+
+    asteroid = new Asteroid(BIG);
+    //asteroid->setColor(0, 1, 0);
+    asteroid->setSpeed(Dot(-0.5, 1));
+    asteroid->setHandling(1);
+    asteroid->setPosition(Dot(-100, 0));
+    asteroid->centralize();
+    asteroids.push_back(asteroid);
+*/
+
     Shot *shot;
     for(int i=0; i<SHOTS_MAX; i++){
         shot = new Shot();
         to_shot.push_back(shot);
     }
+
+    srand(time(NULL));
+
+    setUpLevel(ARCADE, 1);
 }
 
 void Game::setPlayer(Aircraft *p){
     player = p;
 }
 
+void Game::pause(){
+    paused = true;
+}
+
+void Game::continuee(){
+    paused = false;
+}
+
+void Game::pauseOrContinue(){
+    paused = !paused;
+}
+
 Aircraft* Game::getPlayer(){
     return player;
 }
 
-void Game::shoot(Aircraft* aircraft_shooting){
+bool Game::isPaused(){
+    return paused;
+}
+
+
+void Game::resetPlayer(){
+    int model = player->getModel();
+
+    delete player;
+
+    player = new Aircraft(model);
+}
+
+void Game::setUpLevel(int game_mode, int level_number){
+    clearLevel();
+    Asteroid *asteroid;
+
+    int radius = XMAX;
+    int angle = 360/level_number;
+    int d_angle = rand()%angle;
+
+    for(int i=0; i<level_number; i++){
+        asteroid = new Asteroid(BIG);
+        asteroid->setPosition(Dot(radius*cos((i*angle+d_angle)*PI/180), radius*sin((i*angle+d_angle)*PI/180)));
+        asteroid->setSpeed(rand()%360, 0.5+(rand()%150)*0.01);
+        asteroids.push_back(asteroid);
+    }
+
+    /*if(player_dead != NULL){
+        player = player_dead;
+        player_dead = NULL;
+    }*/
+    player->setPosition(Dot(0,0));
+    player->setSpeed(Dot(0,0));
+    player->setDirection(90);
+    player->setLastTimeShooting(-(player->getDelayTimeShooting()));
+
+    continuee();
+    level = level_number;
+    iterations = 0;
+}
+
+void Game::advanceLevel(){
+    setUpLevel(game_mode, level+1);
+}
+
+void Game::clearLevel(){
+    Asteroid *asteroid;
+    Item *item;
+    GameObj *gameobj;
+    Shot *shot;
+
+    /*while(!asteroids.empty()){
+        asteroid = asteroids.back();
+        asteroids.pop_back();
+        delete asteroid;
+    }
+
+    while(!items.empty()){
+        delete items.back();
+        items.pop_back();
+    }
+
+    while(!destroyed.empty()){
+        delete destroyed.back();
+        destroyed.pop_back();
+    }
+    */
+    asteroids.clear();
+    items.clear();
+    destroyed.clear();
+
+    while(!shots.empty()){
+        shot = shots.back();
+        shots.pop_back();
+        to_shot.push_back(shot);
+    }
+}
+
+void Game::shoot(Aircraft* aircraft_to_shot){
     int guns;
     Shot *shot;
     Gun gun;
 
-    Dot aircraft_position = aircraft_shooting->getPosition();
-    float aircraft_direction = aircraft_shooting->getDirection();
+    Dot aircraft_position = aircraft_to_shot->getPosition();
+    float aircraft_direction = aircraft_to_shot->getDirection();
 
-    guns = aircraft_shooting->getGuns();
+    guns = aircraft_to_shot->getGuns();
 
     for(int i=0; i<guns; i++){
-        gun = aircraft_shooting->getGunAt(i);
+        gun = aircraft_to_shot->getGunAt(i);
 
-        //Remove from to_shot list
-        shot = to_shot.back();
-        to_shot.pop_back();
+        if(!to_shot.empty()){
+            //Remove from to_shot list
+            shot = to_shot.back();
+            to_shot.pop_back();
 
-        //set values from aircraft state
-        shot->setPosition(gun.getPosition().transf(aircraft_direction, aircraft_position));
-        shot->setDirection(gun.getDirection() + aircraft_direction);
-        shot->setSpeed(gun.getDirection() + aircraft_direction, MAX_SPEED);
-        shot->setIterationsLeft(aircraft_shooting->getIterationsRangeShooting());
+            //set values from aircraft stateŝ
+            shot->setPosition(gun.getPosition().transf(aircraft_direction, aircraft_position));
+            shot->setDirection(gun.getDirection() + aircraft_direction);
+            shot->setSpeed(gun.getDirection() + aircraft_direction, MAX_SPEED);
+            shot->setIterationsLeft(aircraft_to_shot->getIterationsRangeShooting());
 
-        aircraft_shooting->sumSpeed(shot->getDirection() + 180, 1.0/MAX_SPEED);
+            aircraft_to_shot->sumSpeed(shot->getDirection() + 180, 1.0/MAX_SPEED);
 
-        //colocar na lista shots
-        shots.push_back(shot);
+            //colocar na lista shots
+            shots.push_back(shot);
+
+            player->setLastTimeShooting(iterations);
+        }
     }
-
-    player->setLastTimeShooting(iterations);
 }
 
 void Game::crash(Asteroid *asteroid_crashed){
-    int n;
 
     Item *drop = asteroid_crashed->getItem();
-    std::vector<Asteroid*> *subparts = asteroid_crashed->getParts();
+    std::vector<Asteroid*> crashed_parts;
+    Asteroid *asteroid;
 
     if(drop != NULL){
+        asteroid_crashed->setItem(NULL);
         drop->setPosition(asteroid_crashed->getPosition());
         items.push_back(drop);
     }
@@ -102,12 +212,18 @@ void Game::crash(Asteroid *asteroid_crashed){
     }
 
     //Put in scene all asteroids parts
-    if(subparts != NULL){
+    while(asteroid_crashed->hasParts()){
+        asteroid = asteroid_crashed->popBackPart();
+        asteroid->setPosition(asteroid_crashed->getPosition());
+        asteroids.push_back(asteroid);
+    }
+    /*
+    if(asteroid_crashed.hasParts()){
         for(int i=0; i<subparts->size(); i++){
             subparts->at(i)->setPosition(asteroid_crashed->getPosition());
             asteroids.push_back(subparts->at(i));
         }
-    }
+    }*/
 
     //Put the crashed asteroid to the destroyed list
     destroyed.push_back(asteroid_crashed);
@@ -123,30 +239,35 @@ void Game::removeShot(Shot *obj){
     to_shot.push_back(obj);
 }
 
-void Game::destroy(Polygon* obj){
+void Game::moveToDestroyed(GameObj* obj){
     if(obj == player){
+        player_dead = (Aircraft*)obj;
         player = NULL;
     }
-    destroyed.push_back(obj);
+    else{
+        destroyed.push_back(obj);
+    }
 }
 
 void Game::assignItem(Aircraft *a, Item *it){
     a->catchItem(it);
 }
 
-void Game::iterate(bool boosting, bool turning_left, bool turning_right, bool shooting){
+void Game::iterate(){
     int i, j;
 
     Shot *s;
     Asteroid *a;
 
+
+    // Player shooting
     if(player != NULL){
-        if(shooting && player->canShoot(iterations) && to_shot.size() > 0){
+        if(*(player->getCommandAddress(SHOOT)) && player->canShoot(iterations) && to_shot.size() > 0){
             shoot(player);
         }
     }
 
-    // Crashing Asteroid
+    // Shootings at Asteroids
     for(i=0; i<asteroids.size(); i++){
         for(j=0; j<shots.size(); j++){
             a = asteroids.at(i);
@@ -166,7 +287,12 @@ void Game::iterate(bool boosting, bool turning_left, bool turning_right, bool sh
     // Check for player crashes
     for(std::vector<Asteroid*>::iterator iast = asteroids.begin(); player != NULL && iast != asteroids.end(); iast++){
         if(player->hasPartsInsideOfPolygon(*iast)){
-            destroy(player);
+            if(player->isShielded()){
+                player->damageShield(1);
+            }
+            else{
+                moveToDestroyed(player);
+            }
             break;
         }
     }
@@ -183,29 +309,38 @@ void Game::iterate(bool boosting, bool turning_left, bool turning_right, bool sh
 
 
     // Iterating Objects movements
-    if(player != NULL) player->iterate(boosting, turning_left, turning_right);
+    if(player != NULL) player->iterate();
     for(i=0; i<asteroids.size(); i++){
-        asteroids.at(i)->iterate(false, true, false);
+        asteroids.at(i)->iterate();
     }
     for(i=0; i<items.size(); i++){
-        items.at(i)->iterate(false, false, false);
+        items.at(i)->iterate();
     }
     for(i=0; i<shots.size(); i++){
-        shots.at(i)->iterate(false, false, false);
-        shots.at(i)->reduceIterationsLeft();
+        shots.at(i)->iterate();
     }
 
     // Iterating shots life-time
     for(std::vector<Shot*>::iterator is = shots.begin(); is != shots.end(); is++){
-        if(!((*is)->hasIterationsLeft()) == true){
+        if(!((*is)->hasIterationsLeft())){
             s = *is;
             removeShot(s);
             break;
         }
     }
 
+    if(!destroyed.empty()){
+        //delete destroyed.back();
+        //destroyed.pop_back();
+    }
 
     iterations++;
+
+    // Is level clear
+    if(asteroids.size() == 0){
+        advanceLevel();
+    }
+
 }
 
 void Game::draw(void){
@@ -223,9 +358,12 @@ void Game::draw(void){
         shots.at(i)->draw();
     }
 
-/*    std::string word;
-    word = std::to_string(interceptions);
-    printWord(0, 0, word.c_str(), GLUT_BITMAP_TIMES_ROMAN_10);*/
+
+    glColor3f(0.5, 0.5, 0);
+    std::string word;
+    word = "level ";
+    word += std::to_string(level);
+    printWord(0, 0, word.c_str(), GLUT_BITMAP_TIMES_ROMAN_10);
 }
 
 void Game::printWord(float x, float y, const char *str, void *font){
@@ -239,8 +377,8 @@ void Game::printWord(float x, float y, const char *str, void *font){
 }
 
 Game::~Game(){
-    //delete aircrafts[0];
-    //delete aircrafts[1];
-    //delete aircrafts[2];
+    delete player;
+    delete player_dead;
+    clearLevel();
 }
 
